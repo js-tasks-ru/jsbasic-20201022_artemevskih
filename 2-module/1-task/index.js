a411584b7c3a6027fefcd06f60853c055dccebeb
artemevskih <artemevskih@gmail.com>
2020-11-09 12:21:48 +0300

    Функция суммирования заработных плат

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -5,4 +5,11 @@
  */
 function sumSalary(salaries) {
   // ваш код...
+  let sum = 0;
+
+  for (let salary in salaries) {
+    (typeof salaries[salary]) === 'number' ? sum += salaries[salary] : sum;
+  }
+
+  return sum;
 }
