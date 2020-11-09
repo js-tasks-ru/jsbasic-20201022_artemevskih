093e3c888857e6c23776d332e5aed3f5de706de3
artemevskih <artemevskih@gmail.com>
2020-11-09 13:28:03 +0300

    Калькулятор + Проверка на пустой объект

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,19 @@
 let calculator = {
   // ваш код
+
+  read(x, y) {
+    typeof x === 'number' ? this.a = x : console.log('Not a number');
+    typeof y === 'number' ? this.b = y: console.log('Not a number');
+  },
+
+  sum() {
+    return this.a + this.b;
+  },
+
+  mul() {
+    return this.a * this.b;
+  }
+
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
