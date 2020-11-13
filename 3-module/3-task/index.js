9f775b2826b0807531530cff55e908f2bcb1ef97
artemevskih <artemevskih@gmail.com>
2020-11-13 09:28:30 +0300

    модуль 3-3 через стрелочную функцию

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -5,8 +5,6 @@
 function camelize(str) {
   // ваш код...
   return str.split('-')
-    .map(function(item, index) {
-      return index >= 1 ? item[0].toUpperCase() + item.slice(1) : item;
-    })
+    .map((item, index) => index >= 1 ? item[0].toUpperCase() + item.slice(1) : item)
     .join('');
 }
